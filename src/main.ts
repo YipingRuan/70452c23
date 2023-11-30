e84cd928f09e2391fddf3b81d6ea36c7f021df75
Yiping <YipingRuan@users.noreply.github.com>
2023-11-30 22:18:21 +0800

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './shared/all-exceptions.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  setupSwagger(app);
+
   // Exception -> response handling
   const httpAdapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
-  
+
   await app.listen(3000);
 }
 bootstrap();
+
+function setupSwagger(app: INestApplication<any>) {
+  const options = new DocumentBuilder()
+    .setTitle('My ZUJU Demo API')
+    .setVersion('1.22474487139')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
--- src/match-fixture/match-fixture.controller.ts
+++ src/match-fixture/match-fixture.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { MatchFixtureService } from './match-fixutre.service';
 
 @Controller('match-fixture')
 export class MatchFixtureController {
   constructor(private readonly service: MatchFixtureService) { }
+
+  @Post("fixturesListing")
+  fixturesListing(date: number, timezoneOffset: number) {
+    return {
+      date,
+      timezoneOffset,
+      matches: [],
+      haveNext: true
+    };
+  }
+
+  @Get("fixturesCalendar")
+  fixturesCalendar(year: number, month: number) {
+    return {
+      year,
+      month,
+      matchMask: 1234
+    };
+  }
 
   @Get()
   getHello(): string {
