e8af1dc68c713ef731b087db7306e8e034afc242
Yiping <YipingRuan@users.noreply.github.com>
2023-12-03 09:53:23 +0800

    Add match-fixture module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,33 @@
-import { MiddlewareConsumer, Module, NestModule, Scope } from '@nestjs/common';
+import { Global, MiddlewareConsumer, Module, NestModule, Scope } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MatchFixtureController } from './match-fixture/match-fixture.controller';
-import { MatchFixtureService } from './match-fixture/match-fixutre.service';
 import { PlaygroundController } from './playground/playground.controller';
 import {
   CorrelationIdMiddleware,
   CorrelationModule,
 } from '@evanion/nestjs-correlation-id';
 import { CustomCorrelationIdMiddleware } from './shared/middleware/CustomCorrelationIdMiddleware';
+import { MatchFixtureModule } from './match-fixture/match-fixture.module';
 import { redisClientFactory } from './shared/redisFactory';
 
+@Global()
 @Module({
-  imports: [CorrelationModule.forRoot(), 
-    ConfigModule.forRoot({envFilePath: ['.env.development'],})],
-  controllers: [AppController, MatchFixtureController, PlaygroundController],
+  imports: [
+    CorrelationModule.forRoot(), 
+    ConfigModule.forRoot({envFilePath: ['.env.development'],}), 
+    MatchFixtureModule],
+  controllers: [AppController, PlaygroundController],
   providers: [
-    AppService, 
-    MatchFixtureService, 
-    { 
+    AppService,
+    {
       provide: "RedisClient",
-      useFactory: (config: ConfigService) => redisClientFactory(config), 
+      useFactory: (config: ConfigService) => redisClientFactory(config),
       inject: [ConfigService],
       scope: Scope.DEFAULT,
-    }
+  }
   ],
+  exports: ["RedisClient"]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
