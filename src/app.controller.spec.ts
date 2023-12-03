85a8268865c986f023edfffe8f3e50997a3de2e2
Yiping <YipingRuan@users.noreply.github.com>
2023-12-03 09:24:51 +0800

    Add test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let app: TestingModule;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    app = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
