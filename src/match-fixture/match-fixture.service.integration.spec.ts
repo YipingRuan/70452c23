7a52801a9bfb5a110df6d022c5c5ba91887a9818
Yiping <YipingRuan@users.noreply.github.com>
2023-12-03 19:15:21 +0800

    Adjust test

--- src/match-fixture/match-fixture.service.integration.spec.ts
+++ src/match-fixture/match-fixture.service.integration.spec.ts
@@ -1,21 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MatchFixtureController } from './match-fixture.controller';
 import { MatchFixtureService } from "./match-fixutre.service";
 import { CorrelationModule } from '@evanion/nestjs-correlation-id';
 import { Scope } from '@nestjs/common';
-import { AppService } from '../app.service';
 import { redisClientFactory } from '../shared/redisFactory';
 import * as dayjs from 'dayjs';
 import { CodedError } from '../shared/CodedError';
 
-const resolveController = async () => {
+const resolveService = async () => {
   const module: TestingModule = await Test.createTestingModule({
     imports: [
       CorrelationModule.forRoot(),
       ConfigModule.forRoot({ envFilePath: ['.env.development'], })
     ],
-    controllers: [MatchFixtureController],
+    // controllers: [MatchFixtureController],
     providers: [
       MatchFixtureService,
       {
@@ -27,33 +25,33 @@ const resolveController = async () => {
     ],
   }).compile();
 
-  return await module.resolve(MatchFixtureController);
+  return await module.resolve(MatchFixtureService);
 };
 
 // Assume redis setup is done
 describe('listDailyMatches', () => {
-  let controller: MatchFixtureController;
+  let service: MatchFixtureService;
   // Mock or not mock redis service?
 
-  beforeEach(async () => controller = await resolveController());
+  beforeEach(async () => service = await resolveService());
 
   it('Should return daily matches', async () => {
-    const res = await controller.listDailyMatches({date: "2023-12-05", timezoneOffset: 3});
-    
-    expect(res.matches.length).toEqual(105);
-    expect(res.localDay[0].isSame(dayjs("2023-12-04T19:00:00.000Z"))).toBeTruthy();
-    expect(res.localDay[1].isSame(dayjs("2023-12-05T18:59:59.999Z"))).toBeTruthy();
+    const res = await service.listDailyMatches("2023-12-05", 8);
+
+    expect(res.matches.length).toEqual(103);
+    expect(res.localDay[0].isSame(dayjs("2023-12-04T16:00:00.000Z"))).toBeTruthy();
+    expect(res.localDay[1].isSame(dayjs("2023-12-05T15:59:59.999Z"))).toBeTruthy();
   });
 });
 
 describe('listMonthlyMatchMask', () => {
-  let controller: MatchFixtureController;
+  let service: MatchFixtureService;
 
-  beforeEach(async () => controller = await resolveController());
+  beforeEach(async () => service = await resolveService());
 
   it('Should return daily matches', async () => {
-    const res = await controller.listMonthlyMatchMask({year: 2023, month: 12});
-    
+    const res = await service.listMonthlyMatchMask(2023, 12);
+
     expect(res.mask).toEqual(450912258);
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { MatchFixtureModule } from './../src/match-fixture/match-fixture.module';
+import { RedisClientType } from 'redis';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let redisClient: RedisClientType;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,6 +15,7 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    redisClient = moduleFixture.get("RedisClient");
     await app.init();
   });
 
@@ -23,6 +27,42 @@ describe('AppController (e2e)', () => {
   });
 
   afterAll(async () => {
+    await redisClient.disconnect();
     await app.close();
-  })
+  });
 });
+
+describe('MatchFixture (e2e)', () => {
+  let app: INestApplication;
+  let redisClient: RedisClientType;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, MatchFixtureModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    redisClient = moduleFixture.get("RedisClient");
+    await app.init();
+  });
+
+  it('/matchFixture/listDailyMatches (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/matchFixture/listDailyMatches?date=2013-01-01&timezoneOffset=0')
+      .expect(200)
+      .expect('{"date":"2013-01-01","timezoneOffset":0,"localDay":["2013-01-01T00:00:00.000Z","2013-01-01T23:59:59.999Z"],"matches":[{"id":-1,"time":20130101000000,"tournamentId":0,"homeTeamId":0,"awayTeamId":0,"score":"1:2","isEnded":true,"isLive":false}]}');
+  });
+
+  it('/matchFixture/listMonthlyMatchMask (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/matchFixture/listMonthlyMatchMask?year=2023&month=12')
+      .expect(200)
+      .expect('{"year":2023,"month":12,"mask":450912258}');
+  });
+
+  afterAll(async () => {
+    await redisClient.disconnect();
+    await app.close();
+  });
+});
+
